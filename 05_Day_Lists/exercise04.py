464654aab45d049b5f6fb1749f0ad4c11d24929d
Nicolas Gabriel <nicolasgabriel@usp.br>
2026-03-04 23:44:50 -0300

    exercise 04 add

---
+++ 05_Day_Lists/exercise04.py
@@ -0,0 +1,4 @@
+list = ["nicolas", "joao", "carlos", "ana", "maria"]
+print("the first item: ", list[0])
+print("the middle item: ", list[len(list)/2 + 1])
+print("the last item: ", list[len(list)])
