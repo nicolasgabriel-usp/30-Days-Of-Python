0607f29dabb8c9ee289c925649d734bee67f0670
Nicolas Gabriel <nicolasgabriel@usp.br>
2026-03-14 23:51:56 -0300

    exercise 01 add

---
+++ 04_Day_String/exercise01.py
@@ -0,0 +1,6 @@
+first_string = "Thirty"
+second_string = "Days"
+third_string = "Of"
+fourth_string = "Python"
+full_string = first_string + second_string + third_string + fourth_string
+print(full_string)
